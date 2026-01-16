3b4bef8b89d3dd2275ea486f35929351fa0da9b3
Mohamed Said <mohamedsaidbenyaiche@gmail.com>
2026-01-16 10:53:02 +0100

    Test

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,9 +6,30 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
+    /**
+     * Show all categories
+     */
     public function categories()
-        {
-            return 'electronics OK';
-        }
+    {
+        $categories = Product::select('categorie')
+            ->distinct()
+            ->get();
 
+        return view('electronics-categories', [
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * Show products by category
+     */
+    public function byCategory($category)
+    {
+        $products = Product::where('categorie', $category)->get();
+
+        return view('electronics-products', [
+            'products' => $products,
+            'category' => $category
+        ]);
+    }
 }
