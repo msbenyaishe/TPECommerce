ea2788e73d3d5bd3b1d90e0fdc7e1157fb5fc361
Mohamed Said <mohamedsaidbenyaiche@gmail.com>
2026-02-06 01:08:34 +0100

    Fix

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -43,7 +43,7 @@
                                 <form action="{{ route('cart.remove') }}" method="POST">
                                     @csrf
                                     <input type="hidden" name="id" value="{{ $id }}">
-                                    <button class="msone-btn-remove">Remove</button>
+                                    <button class="msone-btn-remove" onclick="return confirm('Delete this product?')">Remove</button>
                                 </form>
                             </td>
                         </tr>
